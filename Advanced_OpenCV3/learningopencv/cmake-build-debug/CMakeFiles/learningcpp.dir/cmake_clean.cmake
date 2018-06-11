37f0c14d9061319d73f79707fc2ffe6082f3486d
karenyyy <karenye.psu@gmail.com>
2018-06-11 04:51:23 -0400

    update

---
+++ Advanced_OpenCV3/learningopencv/cmake-build-debug/CMakeFiles/learningcpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learningcpp.dir/main.cpp.o"
+  "learningcpp.pdb"
+  "learningcpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learningcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
