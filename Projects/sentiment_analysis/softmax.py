d3dfa383616fb9a78c1f91d6dc25d0208f0e7940
karenyyy <karenye.psu@gmail.com>
2018-06-15 07:32:55 -0400

    update

---
+++ Projects/sentiment_analysis/softmax.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+def softmax(x):
+    """Compute the softmax function for each row of the input x.
+
+
+    Arguments:
+    x -- A N dimensional vector or M x N dimensional numpy matrix.
+
+    Return:
+    x
+    """
+    orig_shape = x.shape
+
+    if len(x.shape) > 1:
+        # Matrix
+        
+        exp_minmax = lambda x: np.exp(x - np.max(x))
+        denom = lambda x: 1.0 / np.sum(x)
+        x = np.apply_along_axis(exp_minmax, 1, x)
+        denominator = np.apply_along_axis(denom, 1, x)
+
+        if len(denominator.shape) == 1:
+            denominator = denominator.reshape((denominator.shape[0], 1))
+
+        x = x * denominator
+        
+    else:
+        # Vector
+        
+        x_max = np.max(x)
+        x = x - x_max
+        numerator = np.exp(x)
+        denominator = 1.0 / np.sum(numerator)
+        x = numerator.dot(denominator)
+        
+
+    assert x.shape == orig_shape
+    return x
+
+
+def test_softmax_basic():
+    """
+    Some simple tests to get you started.
+    Warning: these are not exhaustive.
+    """
+    print("Running basic tests...")
+    test1 = softmax(np.array([1, 2]))
+    print(test1)
+    ans1 = np.array([0.26894142, 0.73105858])
+    assert np.allclose(test1, ans1, rtol=1e-05, atol=1e-06)
+
+    test2 = softmax(np.array([[1001, 1002], [3, 4]]))
+    print(test2)
+    ans2 = np.array([
+        [0.26894142, 0.73105858],
+        [0.26894142, 0.73105858]])
+    assert np.allclose(test2, ans2, rtol=1e-05, atol=1e-06)
+
+    test3 = softmax(np.array([[-1001, -1002]]))
+    print(test3)
+    ans3 = np.array([0.73105858, 0.26894142])
+    assert np.allclose(test3, ans3, rtol=1e-05, atol=1e-06)
+
+    print("You should be able to verify these results by hand!\n")
+
+
+
+
+if __name__ == "__main__":
+    test_softmax_basic()
+
