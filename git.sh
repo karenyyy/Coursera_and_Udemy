c32194c1b006248e9101206436ff3a7d6b78b0ca
karenyyy <karenye.psu@gmail.com>
2018-07-14 01:40:18 -0400

    update

--- git.sh
+++ git.sh
@@ -2,3 +2,6 @@ git config credential.helper store
 git add .
 git commit -m 'update'
 git push -u origin master
+
+## to delete large files as well as cache on master
+# git filter-branch --index-filter '<file-path>' HEAD
