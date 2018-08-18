b10ea999be7ae9c8e560739a05500e2c74ee31c4
Jiarong Ye <33590964+karenyyy@users.noreply.github.com>
2018-08-18 03:54:44 -0400

    Update git.sh

--- git.sh
+++ git.sh
@@ -4,4 +4,4 @@ git commit -m 'update'
 git push -u origin master
 
 ## to delete large files as well as cache on master
-# git filter-branch --index-filter '<file-path>' HEAD
+# git filter-branch --index-filter 'git rm -r --cached --ignore-unmatch <file/dir>' HEAD
